97a5e6f1d7806c05e1e852e3551e093595bdccc7
eletroeduardo <eduardo@eletrogate.com>
2023-11-09 16:56:13 -0300

    altera formato da tabela de roteamento

--- tp2/roteador.py
+++ tp2/roteador.py
@@ -2,17 +2,32 @@ import sys
 import time
 import socket
 import threading
-from struct import pack, unpack
+from struct import unpack
 
 class Roteador:
     def __init__(self, nome_roteador, arquivo_config):
         self.nome_roteador = nome_roteador
-        self.roteadores_na_rede = {}
-        self.vizinhos = {}
-        self.tabela_roteamento = []
+        self.roteadores_na_rede = {}  # [nome]: (IP, port)
+        self.vizinhos = {}            # [nome]: (IP, port)
+        self.tabela_roteamento = {}   # [destino]: Caminho
         self.carregar_config(arquivo_config)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind((self.ip_roteador, self.porta_roteador))
+    
+    class Caminho:
+        def __init__(self, prox, dist):
+            self.prox_passo = prox
+            self.distancia = dist
+
+        def __lt__(self, other):
+            return self.distancia < other.distancia
+        
+        def __gt__(self, other):
+            return self.distancia > other.distancia
+        
+        def __eq__(self, other):
+            return self.distancia == other.distancia
+
 
     def carregar_config(self, arquivo_config):
         with open(arquivo_config, 'r') as arquivo:
@@ -32,16 +47,10 @@ class Roteador:
     
     def eh_pacote_de_configuracao(self, pacote):
         return pacote.decode()[0] in 'CDTEI'
-
-    def extrai_prox_passo(self, destino):
-        for linha in self.tabela_roteamento:
-            destino_tabela, prox_passo, _ = linha.strip().split()
-            if destino == destino_tabela:
-                return prox_passo
             
     def envia_pela_tabela(self, msg, destino):
         msg = b"M" + msg + b" " + destino
-        self.socket.sendto(msg, self.extrai_prox_passo(destino))
+        self.socket.sendto(msg, self.tabela_roteamento[destino].prox_passo)
     
     def executa_configuracao(self, pacote):
         config = chr(pacote[0])
@@ -55,9 +64,8 @@ class Roteador:
             case 'D':
                 self.vizinhos.pop(roteador_referido)
             case 'T':
-                for linha in self.tabela_roteamento:
-                    destino, prox_passo, distancia = linha.strip().split()
-                    print('T' + destino + prox_passo + str(distancia))
+                for destino, caminho in self.tabela_roteamento.items():
+                    print('T' + destino + caminho.prox_passo + str(caminho.distancia))
             case 'E':
                 msg = pacote[34:99]
                 self.envia_pela_tabela(msg, roteador_referido)
@@ -71,12 +79,12 @@ class Roteador:
         return not self.eh_pacote_de_configuracao(pacote)
     
     def destinos_tabela_roteamento(self):
-        return [linha[0] for linha in self.tabela_roteamento]
+        return [str(destino) for destino in self.tabela_roteamento.keys()]
 
     
     def trata_pacote_de_dados(self, pacote, origem):
         # M: mensagem | R: roteamento
-        tipo = unpack(">c", pacote[0])
+        tipo = chr(pacote[0])
         if tipo == 'M':
             msg, destino = pacote[1:].rsplit(maxsplit=1)
             if destino == self.nome_roteador:
@@ -84,18 +92,32 @@ class Roteador:
             elif destino in self.destinos_tabela_roteamento():
                 self.envia_pela_tabela(msg, destino)
         elif tipo == 'R':
-            # é mais eficiente implementar após implementar enviar_info_roteamento
-            pass
+
+            self.tabela_roteamento[origem] = (origem, 1)
+
+            linhas = str(pacote[1:]).split('\n')
+            for [destino, prox_passo, distancia] in linhas:
+                pass
+            """ - """
+                
+
 
     def executar(self):
         while True:
-            pacote, endereco = self.socket.recvfrom(1024)  # tamanho do buffer é 1024 bytes
+            pacote, endereco = self.socket.recvfrom(1024)
             self.lidar_com_pacote(pacote, endereco)
 
+    def formata_tabela_roteamento(self):
+        msg = ''
+        for destino, (prox_passo, distancia) in self.tabela_roteamento.items():
+            linha = ' '.join([destino, prox_passo, str(distancia)])
+            msg += linha + "\n"
+        return msg
+
     def enviar_info_roteamento(self):
-        # Lógica para enviar informações de roteamento para vizinhos
-        msg = 'R' + str(len(self.tabela_roteamento)) + self.tabela_roteamento
-        pass
+        msg = "R\n" + self.formata_tabela_roteamento()
+        for vizinho in self.vizinhos.values():
+            self.socket.sendto(msg, vizinho)
 
     def iniciar_roteamento(self):
         while True:
