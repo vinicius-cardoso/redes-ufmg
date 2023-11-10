55a38d84b35a6c8b62c0c5e058da9e5e6e2aa81b
eletroeduardo <eduardo@eletrogate.com>
2023-11-10 17:01:53 -0300

    muita coisa. nada resolvido. não cabe aqui

--- tp2/roteador.py
+++ tp2/roteador.py
@@ -39,11 +39,11 @@ class Roteador:
                 else:
                     self.roteadores_na_rede[nome] = (ip, int(porta))
 
-    def lidar_com_pacote(self, pacote, endereco):
+    def lidar_com_pacote(self, pacote):
         if self.eh_pacote_de_configuracao(pacote):
             self.executa_configuracao(pacote)
         elif self.eh_pacote_de_dados(pacote):
-            self.trata_pacote_de_dados(self, pacote, endereco)
+            self.trata_pacote_de_dados(pacote)
     
     def eh_pacote_de_configuracao(self, pacote):
         return pacote.decode()[0] in 'CDTEI'
@@ -55,14 +55,18 @@ class Roteador:
     def executa_configuracao(self, pacote):
         config = chr(pacote[0])
         if len(pacote) > 1:
-            roteador_referido = unpack(">32s", pacote[1:34])
+            roteador_referido = unpack(">32s", pacote[1:34])[0].rstrip(b'\x00').decode()
         else:
             roteador_referido = 0
         match config:
             case 'C':
                 self.vizinhos[roteador_referido] = self.roteadores_na_rede[roteador_referido]
+                self.tabela_roteamento[roteador_referido] = self.Caminho(roteador_referido, 1)
             case 'D':
                 self.vizinhos.pop(roteador_referido)
+                for caminho in self.tabela_roteamento.values():
+                    if caminho.prox_passo == roteador_referido:
+                        del self.roteadores_na_rede[roteador_referido]
             case 'T':
                 for destino, caminho in self.tabela_roteamento.items():
                     print('T' + destino + caminho.prox_passo + str(caminho.distancia))
@@ -81,8 +85,7 @@ class Roteador:
     def destinos_tabela_roteamento(self):
         return [str(destino) for destino in self.tabela_roteamento.keys()]
 
-    
-    def trata_pacote_de_dados(self, pacote, origem):
+    def trata_pacote_de_dados(self, pacote):
         # M: mensagem | R: roteamento
         tipo = chr(pacote[0])
         if tipo == 'M':
@@ -92,35 +95,39 @@ class Roteador:
             elif destino in self.destinos_tabela_roteamento():
                 self.envia_pela_tabela(msg, destino)
         elif tipo == 'R':
+            origem, pacote = pacote[1:].split(maxsplit=1)
 
-            self.tabela_roteamento[origem] = (origem, 1)
+            self.tabela_roteamento[origem] = self.Caminho(origem, 1)
 
-            linhas = str(pacote[1:]).split('\n')
-            for [destino, prox_passo, distancia] in linhas:
+            linhas = pacote.splitlines()
+            for linha in linhas:
+                [destino, prox_passo, distancia] = linha.decode().split()
                 if destino not in self.tabela_roteamento:
-                    self.tabela_roteamento[destino] = self.Caminho(prox_passo, distancia + 1)
+                    self.tabela_roteamento[destino] = self.Caminho(prox_passo, int(distancia) + 1)
                 elif self.tabela_roteamento[destino].prox_passo == origem:
-                    self.tabela_roteamento[destino] = (origem, distancia + 1)
+                    self.tabela_roteamento[destino] = self.Caminho(origem, distancia + 1)
                 else:
-                    self.tabela_roteamento[destino] = min(self.Caminho(prox_passo, distancia + 1), self.tabela_roteamento[destino])
+                    self.tabela_roteamento[destino] = min(self.Caminho(prox_passo, int(distancia) + 1), self.tabela_roteamento[destino])
   
 
     def executar(self):
         while True:
-            pacote, endereco = self.socket.recvfrom(1024)
-            self.lidar_com_pacote(pacote, endereco)
+            pacote, _ = self.socket.recvfrom(1024)
+            self.lidar_com_pacote(pacote)
 
     def formata_tabela_roteamento(self):
         msg = ''
-        for destino, (prox_passo, distancia) in self.tabela_roteamento.items():
-            linha = ' '.join([destino, prox_passo, str(distancia)])
-            msg += linha + "\n"
+        for (destino, caminho) in self.tabela_roteamento.items():
+            """ """
+            print(destino, caminho.prox_passo)
+            linha = ' '.join([destino, caminho.prox_passo, str(caminho.distancia)])
+            msg += linha
         return msg
 
     def enviar_info_roteamento(self):
-        msg = "R\n" + self.formata_tabela_roteamento()
+        msg = "R" + self.nome_roteador + " " + self.formata_tabela_roteamento()
         for vizinho in self.vizinhos.values():
-            self.socket.sendto(msg, vizinho)
+            self.socket.sendto(msg.encode(), vizinho)
 
     def iniciar_roteamento(self):
         while True:
@@ -137,6 +144,7 @@ def main():
 
     roteador = Roteador(nome_roteador, arquivo_config)
     roteador.executar()
+    roteador.socket.close()
 
 if __name__ == "__main__":
     main()
