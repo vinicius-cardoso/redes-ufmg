f62c6ce06c6d1b5dbb6fedb35fc309499b218961
eletroeduardo <eduardo@eletrogate.com>
2023-11-27 14:15:53 -0300

    avança na implementação do servidor

--- tp3/sala.py
+++ tp3/sala.py
@@ -1,7 +1,31 @@
 from sys import argv
+import grpc
+import sala_pb2 as chat
+import sala_pb2_grpc as rpc
+from concurrent import futures
+from time import sleep
 
-class salaServicer():
-  pass
+class SalaServidor(rpc.salaServicer):
+    def __init__(self):
+        self.programas = []
+
+    def registra_entrada(self, request, context):
+        return super().registra_entrada(request, context)
+    
+    def registra_saida(self, request, context):
+        return super().registra_saida(request, context)
+    
+    def lista(self, request, context):
+        return super().lista(request, context)
+    
+    def finaliza_registro(self, request, context):
+        return super().finaliza_registro(request, context)
+    
+    def termina(self, request, context):
+        return super().termina(request, context)
+    
+    def envia(self, request, context):
+        return super().envia(request, context)
 
 def main():
     if len(argv) != 2:
@@ -9,6 +33,13 @@ def main():
         return
 
     numero_porto = argv[1]
+
+    servidor = grpc.server(futures.ThreadPoolExecutor())
+    rpc.add_ChatServerServicer_to_server(SalaServidor(), servidor)
+    servidor.add_insecure_port('[::]:' + numero_porto)
+    servidor.start()
+    while True:
+        sleep(float('inf'))
 
 if __name__ == "__main__":
     main()
